17017f568e8bae70028c321ae48fa86e0f742281
SH9480P <realsehwan@gmail.com>
2024-12-28 11:58:44 +0900

    add typechecked eslint configs and turn into mjs file

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { node } from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+    { ignores: ['dist/*'] },
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    // @ts-ignore
+    eslintConfigPrettier,
+    {
+        files: ['**/*.js'],
+        extends: [tseslint.configs.disableTypeChecked],
+    },
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            globals: node,
+        },
+        rules: {
+            '@typescript-eslint/no-var-requires': 'off',
+        },
+    },
+);
